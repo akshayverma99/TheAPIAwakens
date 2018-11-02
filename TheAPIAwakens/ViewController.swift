81eb56136c632ecbb698360e736bd036940ea665
Akshay Verma <AkshayVerma@akshays-macbook-air.local>
2018-11-02 15:22:38 -0400

    so much autolayout stuff

--- TheAPIAwakens/ViewController.swift
+++ TheAPIAwakens/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func segue(_ sender: UIButton) {
+        switch sender.tag{
+            case 1,2,3: performSegue(withIdentifier: "shift", sender: nil)
+            default: print("testicles")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let button = sender as? UIButton else {  return  }
+        
+        switch button.tag{
+            case 1: print("")
+            case 2: print("")
+            case 3: print("")
+        default: print("testicles")
+        }
+        
+    }
+    
 }
 
